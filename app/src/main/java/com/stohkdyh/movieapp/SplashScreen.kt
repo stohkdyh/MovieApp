9df95b95a6adc59b369b7806fc06fba1c545da4f
stohkdyh <adityaekapret@gmail.com>
2023-03-13 20:42:23 +0700

    init

---
+++ app/src/main/java/com/stohkdyh/movieapp/SplashScreen.kt
@@ -0,0 +1,27 @@
+package com.stohkdyh.movieapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+
+@Suppress("DEPRECATION")
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        val backgroundImage:ImageView = findViewById(R.id.SplashScreenImage)
+        val sideAnimation = AnimationUtils.loadAnimation(this, androidx.appcompat.R.anim.abc_fade_in)
+        backgroundImage.startAnimation(sideAnimation)
+
+        Handler().postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
+
+    }
+}
