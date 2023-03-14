a1c267c8ec1b16159783b6b059fd34802e2bbc96
stohkdyh <adityaekapret@gmail.com>
2023-03-14 17:21:01 +0700

    init

--- app/src/main/java/com/stohkdyh/movieapp/DashboardActivity.kt
+++ app/src/main/java/com/stohkdyh/movieapp/DashboardActivity.kt
@@ -3,7 +3,7 @@ package com.stohkdyh.movieapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.stohkdyh.movieapp.databinding.ActivityMainBinding
+import com.stohkdyh.movieapp.databinding.ActivityDashboardBinding
 
 class DashboardActivity : AppCompatActivity() {
     private lateinit var binding: ActivityDashboardBinding
@@ -19,7 +19,7 @@ class DashboardActivity : AppCompatActivity() {
             }
 
             with(rvFilm) {
-                adapter = FilmAdapter(listOf(Film("Doraemon", "2019-08-03", "9.2", R.drawable.image_ex_film), Film("Avangers End Game", "2019-08-03", "9.2", R.drawable.image_ex_film)))
+                adapter = FilmAdapter(listOf(Film("Doraemon", "2019-08-03", "9.2", R.drawable.it), Film("Avangers End Game", "2019-08-03", "9.2", R.drawable.split), Film("Doraemon", "2019-08-03", "9.2", R.drawable.truman) ))
                 layoutManager = LinearLayoutManager(this@DashboardActivity)
             }
         }
